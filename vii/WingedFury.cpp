2451baf0358d1284296563d559ad77d0d190edd6
OAT <64010860@kmitl.ac.th>
2021-10-05 14:33:26 +0700

    improved

--- vii/WingedFury.cpp
+++ vii/WingedFury.cpp
@@ -80,6 +80,12 @@ void clear_bullet(int x, int y, int bulletStatus)
 	printf("   ");
 }
 
+void draw_enemy(int x, int y)
+{
+	gotoxy(x, y);
+	printf("M");
+}
+
 void map_generate()
 {
 	int i,j;
@@ -118,25 +124,63 @@ void map_generate()
 void playerhealth(int php)
 {
 	gotoxy(0, 23);
-	setcolor(3, 0);
+	setcolor(2, 0);
 	printf("HP : %d", php);
+
+	gotoxy(25, 10);
+	printf("<");
+	gotoxy(25, 15);
+	printf("<");
+	gotoxy(25, 18);
+	printf("<");
+
 }
+int RandomX()
+{
+	int x;
+	x = rand();
+	if (x > 20 && x < 57)
+	{
+		return x;
+	}
+	else
+	{
+		RandomX();
+	}
+}
+
+int RandomY()
+{
+	int y;
+	y = rand();
+	if (y > 1 && y < 19)
+	{
+		return y;
+	}
+	else
+	{
+		RandomY();
+	}
+}
+
 
 
 int main()
 {
 	setcursor(0);
+	srand(time(NULL));
 	struct enemy
 	{
+		int status=0;
 		int x;
 		int y;
-		int hp = 10;
-	};enemy enemy[10];
+		int hp = 2;
+	};enemy enemy[3];
 	char ch = ' ';
 	int x = 12, y = 10;
 	int bulletx[5], bullety[5];
 	int bulletStatus[5];
-	int PlayerHP = 3;
+	int PlayerHP = 3, OldPlayerHP = 3;
 	for (int i = 0; i <= 5; i++)
 	{
 		bulletStatus[i] = 0;
@@ -146,6 +190,7 @@ int main()
 	setcolor(7, 0);
 	plane(x, y);
 	do {
+		setcolor(7, 0);
 		if (_kbhit()) 
 		{
 			ch = _getch();
@@ -157,35 +202,35 @@ int main()
 			{
 				Beep(700, 50);
 				bulletStatus[0] = 1;
-				bulletx[0] = x + 8;
+				bulletx[0] = x + 7;
 				bullety[0] = y + 1;
 			}
 			else if (bulletStatus[1] == 0 && ch == ' ')
 			{
 				Beep(700, 50);
 				bulletStatus[1] = 1;
-				bulletx[1] = x + 8;
+				bulletx[1] = x + 7;
 				bullety[1] = y + 1;
 			}
 			else if (bulletStatus[2] == 0 && ch == ' ')
 			{
 				Beep(700, 50);
 				bulletStatus[2] = 1;
-				bulletx[2] = x + 8;
+				bulletx[2] = x + 7;
 				bullety[2] = y + 1;
 			}
 			else if (bulletStatus[3] == 0 && ch == ' ')
 			{
 				Beep(700, 50);
 				bulletStatus[3] = 1;
-				bulletx[3] = x + 8;
+				bulletx[3] = x + 7;
 				bullety[3] = y + 1;
 			}
 			else if (bulletStatus[4] == 0 && ch == ' ')
 			{
 				Beep(700, 50);
 				bulletStatus[4] = 1;
-				bulletx[4] = x + 8;
+				bulletx[4] = x + 7;
 				bullety[4] = y + 1;
 			}
 
@@ -193,18 +238,39 @@ int main()
 			fflush(stdin);
 		}
 
+		/*if (cursor(x + 8, y + 1) == '<')
+		{
+			PlayerHP-=1;
+		}
+		if (cursor(x + 6, y) == '<')
+		{
+			PlayerHP -= 1;
+		}*/
+
+		if (enemy[0].status == 1)
+		{
+			//enemy[0].status == 1;
+			enemy[0].x = RandomX();
+			enemy[0].y = RandomY();
+			draw_enemy(enemy[0].x, enemy[0].y);
+
+
+
+		}
+
 
 		if (bulletStatus[0] == 1)
 		{
 
 			clear_bullet(bulletx[0], bullety[0], 0);
-			if (bulletx[0] == 50)
+			if (bulletx[0] == 57)
 			{
 				bulletStatus[0] = 0;
 			}
-			else if (cursor(bulletx[0], bullety[0] - 1) == '*')
+			else if (cursor(bulletx[0]+3, bullety[0]) == '<')
 			{
 				bulletStatus[0] = 0;
+
 			}
 			else
 			{
@@ -215,11 +281,11 @@ int main()
 		if (bulletStatus[1] == 1)
 		{
 			clear_bullet(bulletx[1], bullety[1], 1);
-			if (bulletx[1] == 50)
+			if (bulletx[1] == 57)
 			{
 				bulletStatus[1] = 0;
 			}
-			else if (cursor(bulletx[1], bullety[1] - 1) == '*')
+			else if (cursor(bulletx[1]+3, bullety[1] - 1) == '*')
 			{
 
 				bulletStatus[1] = 0;
@@ -232,11 +298,11 @@ int main()
 		if (bulletStatus[2] == 1)
 		{
 			clear_bullet(bulletx[2], bullety[2], 2);
-			if (bulletx[2] == 50)
+			if (bulletx[2] == 57)
 			{
 				bulletStatus[2] = 0;
 			}
-			else if (cursor(bulletx[2], bullety[2] - 1) == '*')
+			else if (cursor(bulletx[2]+3, bullety[2] - 1) == '*')
 			{
 
 				bulletStatus[2] = 0;
@@ -250,11 +316,11 @@ int main()
 		if (bulletStatus[3] == 1)
 		{
 			clear_bullet(bulletx[3], bullety[3], 3);
-			if (bulletx[3] == 50)
+			if (bulletx[3] == 57)
 			{
 				bulletStatus[3] = 0;
 			}
-			else if (cursor(bulletx[3], bullety[3] - 1) == '*')
+			else if (cursor(bulletx[3]+3, bullety[3] - 1) == '*')
 			{
 				bulletStatus[3] = 0;
 			}
@@ -266,11 +332,11 @@ int main()
 		if (bulletStatus[4] == 1)
 		{
 			clear_bullet(bulletx[4], bullety[4], 4);
-			if (bulletx[4] == 50)
+			if (bulletx[4] == 57)
 			{
 				bulletStatus[4] = 0;
 			}
-			else if (cursor(bulletx[4], bullety[4] - 1) == '*')
+			else if (cursor(bulletx[4]+3, bullety[4] - 1) == '*')
 			{
 
 				bulletStatus[4] = 0;
@@ -279,6 +345,13 @@ int main()
 			{
 				draw_bullet(++bulletx[4], bullety[4], 4);
 			}
+		}
+
+		if (PlayerHP < OldPlayerHP)
+		{
+			playerhealth(PlayerHP);
+			OldPlayerHP = PlayerHP;
+
 		}
 		Sleep(100);
 	} while (ch != 'x');
